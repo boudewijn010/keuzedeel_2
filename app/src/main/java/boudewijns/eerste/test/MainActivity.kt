82048d2e81cd1ef5efb0ceaf23c6b686b9507432
Boudewijn010 <Boudewijnvanmdamme@gmail.com>
2025-02-18 09:59:25 +0100

    eerste les

--- app/src/main/java/boudewijns/eerste/test/MainActivity.kt
+++ app/src/main/java/boudewijns/eerste/test/MainActivity.kt
@@ -9,12 +9,13 @@ import androidx.core.view.WindowInsetsCompat
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+    var teller = 0
+
+    var gebruikersnaam = "Wilma Flintstone"
+
+    fun geefBericht(text: String) : String {
+        return text
     }
 }
